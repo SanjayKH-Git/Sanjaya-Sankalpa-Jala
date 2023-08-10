ce91df86a924679392487356e7dd8a7e8b347af2
SanjayKH-Git <56336350+SanjayKH-Git@users.noreply.github.com>
2023-08-10 08:18:12 +0000

    initial commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,18 +2,19 @@ import React, { useState } from 'react'
 import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
-import {  FaLinkedinIn } from "react-icons/fa";
-import { SiGithub,SiGmail } from "react-icons/si";
-import {logo} from "../../assets/index"
+import { FaLinkedinIn } from "react-icons/fa";
+import { SiGithub, SiGmail } from "react-icons/si";
+import { logo } from "../../assets/index"
 import { navLinksdata } from '../../constants';
 
 const Navbar = () => {
-  const [showMenu, setShowMenu]=useState(false)
+  const [showMenu, setShowMenu] = useState(false)
   return (
-    <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
-      <div>
-        <img src={logo} alt="logo" />
+    <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px]">
+      <div className="w-16 h-16 rounded-full">
+        <img className="rounded-full" src={logo} alt="logo" />
       </div>
+      <h1 class="mb-8 pt-8 text-lg font-extrabold items-center text-gray-900 dark:text-white md:text-2xl lg:text-3xl"><span class="p-2 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Sanjaya-Sankalpa-Jala</span></h1>
       <div>
         <ul className="hidden mdl:inline-flex items-center gap-6 lg:gap-10">
           {navLinksdata.map(({ _id, title, link }) => (
@@ -42,11 +43,11 @@ const Navbar = () => {
         </span>
         {showMenu && (
           <div className="w-[80%] h-screen overflow-scroll absolute top-0 left-0 bg-gray-900 p-4 scrollbar-hide">
-            <div className="flex flex-col gap-8 py-2 relative">
+            <div className="flex flex-col gap-8 py-2  relative">
               <div>
-                <img className="w-32" src={logo} alt="logo" />
+                <img className="w-32 rounded-xl border-yellow-400 border-4" src={logo} alt="logo" />
                 <p className="text-sm text-gray-400 mt-2">
-                "I am a passionate MERN stack developer and machine learning enthusiast. With a strong foundation in web development and a keen eye for data, I create innovative solutions that bridge technology and user experience."
+                  "I have prolific experience working on a diverse range of projects and freelance contracts in the fields of web development, data science, machine learning, deep learning, and DBMS. For instance, I have developed responsive and user-friendly websites, built predictive models to extract insights from complex data sets, and utilized deep learning techniques to solve computer vision problems."
                 </p>
               </div>
               <ul className="flex flex-col gap-4">
