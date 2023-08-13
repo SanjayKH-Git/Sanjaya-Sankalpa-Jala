ecbfd47de25046a1e28f97d1fdd8f767f550d8d1
SanjayKH-Git <56336350+SanjayKH-Git@users.noreply.github.com>
2023-08-13 17:11:04 +0000

    achievements Commit

--- src/components/resume/ResumeCard.js
+++ src/components/resume/ResumeCard.js
@@ -21,7 +21,7 @@ const ResumeCard = ({ title, subTitle, year = "", result, des }) => {
             </p>
           </div>
           <div>
-            <p className="px-4 py-2 text-designColor bg-black bg-opacity-25 rounded-lg flex justify-center items-center shadow-shadowOne text-sm font-medium">
+            <p className="px-4 py-2 text-designColor bg-black bg-opacity-25 rounded-lg flex justify-center items-center shadow-shadowOne text-sm font-bold">
               {result}
             </p>
           </div>
