62b1011c0939579a021a39cf1b5c60b21889e0d6
SanjayKH-Git <56336350+SanjayKH-Git@users.noreply.github.com>
2023-08-14 13:29:46 +0000

    pre complete

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -16,7 +16,7 @@ const Resume = () => {
         <Title title="WORK EXPERIENCE" des="My Resume" />
       </div>
       <div>
-        <ul className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+        <ul className="w-full grid grid-cols-2 md:grid-cols-2 xl:grid-cols-4">
           <li
             onClick={() =>
               setEducationData(false) &
