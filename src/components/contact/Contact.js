485f1229e8a2bbcac462e4508b6b9071f506af93
SanjayKH-Git <56336350+SanjayKH-Git@users.noreply.github.com>
2023-09-11 08:56:48 +0000

    Time changes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,4 +1,5 @@
-import React,{useState} from 'react'
+// import React,{useState} from 'react'
+import React, { useState, useEffect } from 'react';
 import Title from '../layouts/Title';
 import ContactLeft from './ContactLeft';
 
@@ -45,6 +46,57 @@ const Contact = () => {
       setMessage("");
     }
   };
+
+  const [currentTime, setCurrentTime] = useState(new Date().toUTCString());
+  const [istTime, setIstTime] = useState(
+    new Date().toLocaleTimeString('en-US', {
+      timeZone: 'Asia/Kolkata',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true,
+    })
+  );
+  const [isAvailable, setIsAvailable] = useState(false);
+
+  useEffect(() => {
+    const utcInterval = setInterval(() => {
+      const now = new Date();
+      const utcHours = now.getUTCHours();
+      const utcMinutes = now.getUTCMinutes();
+
+      // Convert current time to minutes since midnight
+      const currentTimeInMinutes = utcHours * 60 + utcMinutes;
+
+      // Calculate the minutes for 2:30 AM and 3:30 PM UTC
+      const startTimeInMinutes = 2 * 60 + 30;
+      const endTimeInMinutes = 15 * 60 + 30;
+
+      // Check if the current time is within the specified range
+      setIsAvailable(
+        currentTimeInMinutes >= startTimeInMinutes &&
+        currentTimeInMinutes <= endTimeInMinutes
+      );
+
+      setCurrentTime(now.toUTCString());
+    }, 1000);
+
+    const istInterval = setInterval(() => {
+      setIstTime(
+        new Date().toLocaleTimeString('en-US', {
+          timeZone: 'Asia/Kolkata',
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: true,
+        })
+      );
+    }, 1000);
+
+    return () => {
+      clearInterval(utcInterval);
+      clearInterval(istInterval);
+    };
+  }, []);
+
   return (
     <section
       id="contact"
@@ -57,6 +109,34 @@ const Contact = () => {
         <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
           <ContactLeft />
           <div className="w-full lgl:w-[60%] h-full py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] flex flex-col gap-8 p-4 lgl:p-8 rounded-lg shadow-shadowOne">
+            <table className="">
+              <tr>
+                <td className="text-center">
+                  <div className="border rounded-xl">
+                    <b className="text-sm">Usual Active Hours: </b> <br />
+                    <b className="text-purple-400">2:30 AM To 3:30 PM ( UTC/GMT )</b><br />
+                    <b className="text-yellow-200 text-sm">8:00 AM To 9:00 PM ( IST )</b>
+                  </div>
+                </td>
+                <td className="text-center">
+                  <div className="border rounded-xl">
+                    <b className="text-sm">Current Time: </b> <br />
+                    <b className="text-blue-400">{currentTime}</b>
+                    <br />
+                    <b className="text-yellow-400 text-sm"> {istTime} ( IST )</b>
+                  </div>
+                </td>
+              </tr>
+            </table>
+
+            <div className="text-center text-xl">
+              {isAvailable ? (
+                <b className="text-green-400">Available</b>
+              ) : (
+                <b className="text-red-600">Offline</b>
+              )}
+            </div>
+
             <form className="w-full flex flex-col gap-4 lgl:gap-6 py-2 lgl:py-5">
               {errMsg && (
                 <p className="py-3 bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne text-center text-orange-500 text-base tracking-wide animate-bounce">
@@ -76,10 +156,9 @@ const Contact = () => {
                   <input
                     onChange={(e) => setUsername(e.target.value)}
                     value={username}
-                    className={`${
-                      errMsg === "Username is required!" &&
+                    className={`${errMsg === "Username is required!" &&
                       "outline-designColor"
-                    } contactInput`}
+                      } contactInput`}
                     type="text"
                   />
                 </div>
@@ -90,10 +169,9 @@ const Contact = () => {
                   <input
                     onChange={(e) => setPhoneNumber(e.target.value)}
                     value={phoneNumber}
-                    className={`${
-                      errMsg === "Phone number is required!" &&
+                    className={`${errMsg === "Phone number is required!" &&
                       "outline-designColor"
-                    } contactInput`}
+                      } contactInput`}
                     type="text"
                   />
                 </div>
@@ -105,10 +183,9 @@ const Contact = () => {
                 <input
                   onChange={(e) => setEmail(e.target.value)}
                   value={email}
-                  className={`${
-                    errMsg === "Please give your Email!" &&
+                  className={`${errMsg === "Please give your Email!" &&
                     "outline-designColor"
-                  } contactInput`}
+                    } contactInput`}
                   type="email"
                 />
               </div>
@@ -119,10 +196,9 @@ const Contact = () => {
                 <input
                   onChange={(e) => setSubject(e.target.value)}
                   value={subject}
-                  className={`${
-                    errMsg === "Plese give your Subject!" &&
+                  className={`${errMsg === "Plese give your Subject!" &&
                     "outline-designColor"
-                  } contactInput`}
+                    } contactInput`}
                   type="text"
                 />
               </div>
@@ -133,9 +209,8 @@ const Contact = () => {
                 <textarea
                   onChange={(e) => setMessage(e.target.value)}
                   value={message}
-                  className={`${
-                    errMsg === "Message is required!" && "outline-designColor"
-                  } contactTextArea`}
+                  className={`${errMsg === "Message is required!" && "outline-designColor"
+                    } contactTextArea`}
                   cols="30"
                   rows="8"
                 ></textarea>
